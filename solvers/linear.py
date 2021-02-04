8de26484f56104e04aff8cb26a94e66607466941
Andrew Miller <avmiller@outlook.com>
2021-02-03 20:50:40 -0500

    Changing license to Apache 2.0 to make it easier/safer to borrow code from other projects/repos. Adding AWS Braket QPE utilities.

--- solvers/linear.py
+++ solvers/linear.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from abc import ABC, abstractmethod
 
+import torch
 from qiskit.aqua import QuantumInstance
 from qiskit.aqua.algorithms import HHL, NumPyLSsolver
 from qiskit.aqua.components.eigs import EigsQPE
@@ -21,7 +22,7 @@ class AbstractLinearSolver(ABC):
     def solve(self, matrix, vector):
         """
         Solves equations of the form A|x〉=|b〉
-        :param matrix: A - matrix corresponding to equation coefficients
+        :param matrix: A - square matrix corresponding to equation coefficients
         :param vector: b - vector representing the desired product
         :return: x - solution vector
         """
@@ -31,8 +32,11 @@ class AbstractLinearSolver(ABC):
 class QiskitLinearSolver(AbstractLinearSolver):
     """
     Package for solving linear systems of equations using Qiskit HHL algorithm. The code in this class is borrowed from
-    Qiskit book and is under Apache License 2.0.
+    Qiskit textbook: https://github.com/qiskit-community/qiskit-textbook
+    http://www.apache.org/licenses/LICENSE-2.0
+    Copyright 2019 IBM and its contributors.
     """
+
     def __init__(self,
                  backend: BaseBackend,
                  num_ancillae: int = 5,
@@ -57,7 +61,7 @@ class QiskitLinearSolver(AbstractLinearSolver):
                        num_ancillae=num_ancillae,
                        expansion_mode='suzuki',
                        expansion_order=2,
-                       evo_time=np.pi*3/4,
+                       evo_time=np.pi * 3 / 4,
                        negative_evals=negative_evals,
                        ne_qfts=ne_qfts)
 
@@ -86,3 +90,25 @@ class NumPyLinearSolver(AbstractLinearSolver):
     def solve(self, matrix, vector):
         result = NumPyLSsolver(matrix, vector).run()
         return result['solution']
+
+
+class BraketLinearSolver(AbstractLinearSolver):
+    def solve(self, matrix, vector):
+        # TODO: Implement HHL using QPE example form https://github.com/aws/amazon-braket-examples
+        # HHL is described in https://qiskit.org/textbook/ch-applications/hhl_tutorial.html#2.-The-HHL-algorithm
+        # This appears to be straight forward application of QPE with
+        raise NotImplementedError
+
+
+class TorchLinearSolver(AbstractLinearSolver):
+    def __init__(self, device: torch.device = None):
+        if device is None and torch.cuda.is_available():
+            self.device = torch.device('cuda')
+        else:
+            self.device = torch.device('cpu')
+
+    def solve(self, matrix, vector):
+        a = torch.tensor(matrix, device=self.device).t()
+        b = torch.tensor([vector], device=self.device).t()
+        x, lu = torch.solve(b, a)
+        return x.t().tolist()[0]
